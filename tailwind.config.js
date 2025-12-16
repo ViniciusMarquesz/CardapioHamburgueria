06102927058093495481090d39e17075c8748bd4
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2025-12-16 11:36:16 -0300

    chore: configure Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.html", "./**/*.js"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        home: "url('../assets/bg.png')",
+      },
+    },
+  },
+  plugins: [],
+};
